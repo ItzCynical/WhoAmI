a2b2e7e50ffe96458608fbc5f86ef95cc78b99ce
Xavier Chua <xavierc.1998@gmail.com>
2017-11-19 00:26:19 -0800

    Added unity project

---
+++ Writing39BQuiz/Assets/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public Question[] questions;
+	private static List<Question> unansweredQuestions;
+
+	private Question currentQuestion;
+
+	[SerializeField]
+	private Text scenarioText;
+
+	[SerializeField]
+	private Text choice1Text;
+
+	[SerializeField]
+	private Text choice2Text;
+
+	[SerializeField]
+	private float timeBetweenQuestions = 1f;
+
+
+	void Start() 
+	{
+		if (unansweredQuestions == null || unansweredQuestions.Count == 0) 
+		{
+			unansweredQuestions = questions.ToList<Question> ();
+		}
+
+		SetCurrentQuestion();
+	}
+
+	void SetCurrentQuestion ()
+	{
+		int randomQuestionIndex = Random.Range (0, unansweredQuestions.Count);
+		currentQuestion = unansweredQuestions [randomQuestionIndex];
+
+		scenarioText.text = currentQuestion.scenario;
+		choice1Text.text = currentQuestion.choice1;
+		choice2Text.text = currentQuestion.choice2;
+
+
+	}
+
+	IEnumerator TransitionToNextQuestion () {
+		unansweredQuestions.Remove (currentQuestion);
+
+		yield return new WaitForSeconds (timeBetweenQuestions);
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+
+
+	} 
+
+	public void UserSelectTrue()
+	{
+		
+	}
+}
